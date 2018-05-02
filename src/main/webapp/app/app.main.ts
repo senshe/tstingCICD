015e94c55e977f298ced8e4c9af28d00064cbfe9
senshe <bastamiebou@gmail.com>
2018-05-02 12:00:41 +0000

    inital commite ,all component all configured

--- src/main/webapp/app/app.main.ts
+++ src/main/webapp/app/app.main.ts
@@ -1,13 +1,14 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { ProdConfig } from './blocks/config/prod.config';
 import { WesitAppModule } from './app.module';
-
+import { AppModule } from './wesitdashboard/app.module';
+ 
 ProdConfig();
 
 if (module['hot']) {
     module['hot'].accept();
 }
 
-platformBrowserDynamic().bootstrapModule(WesitAppModule)
+platformBrowserDynamic().bootstrapModule(AppModule)
 .then((success) => console.log(`Application started`))
 .catch((err) => console.error(err));
